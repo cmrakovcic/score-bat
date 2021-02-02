8846c528120a2bda8baf2eeab081807573f1e7a7
Christopher Mrakovcic <cmrakovcic@aol.com>
2021-02-02 13:46:43 -0500

    Done

--- lib/score_bat/api.rb
+++ lib/score_bat/api.rb
@@ -1,9 +1,3 @@
-# require 'net/http'
-# require 'open-uri'
-# require 'json'
-# require 'awesome_print'
-# require 'pry'
-
 class API
 
   def self.fetch_videos
@@ -21,20 +15,3 @@ class API
     end
   end
 end
-
-#     URL = "https://www.scorebat.com/video-api/v1/"
-
-#   def get_videos
-#     uri = URI.parse(URL)
-#     response = Net::HTTP.get_response(uri)
-#     json = JSON.parse(response.body)
-#   end
-
-
-#   def video_titles(json)
-#     videos = []
-#       json.collect do |video|
-#         videos << video["title"]
-#     end
-#   end
-# end
